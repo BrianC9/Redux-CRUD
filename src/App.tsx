ab10b2a1a964ddb25f90da15ed053f4369fc42d3
Bryan Cusme <bryan.cusme@devoteam.com>
2023-04-23 18:13:33 +0200

    set-up dependencies and workspace

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import UserList from "./components/UserList";
+
+function App() {
+	return (
+		<>
+			<h1>Redux CRUD</h1>
+			<UserList />
+		</>
+	);
+}
+
+export default App;
