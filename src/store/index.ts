c27ca9d3f90a38731f6d0e6f51a8b9054e699ba2
Bryan Cusme <bryan.cusme@devoteam.com>
2023-04-26 13:09:16 +0200

    add: explanation about middleware flow

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Middleware, configureStore } from "@reduxjs/toolkit";
 import usersReducer from "./users/slice";
+
+const persistanceLocalStorageMiddleware: Middleware =
+	(store) => (next) => (action) => {
+		console.log("____Start of middleware persistanceLocalStorageMiddleware_________");
+		console.log("state ANTES de la acción: ",store.getState());
+		console.log("action que hemos enviado:",action);
+		next(action);
+		console.log("state DESPUÉS de procesar la acción", store.getState());
+		console.log("------End of middleware persistanceLocalStorageMiddleware-----");
+
+	};
+
 export const store = configureStore({
 	reducer: {
 		users: usersReducer,
 	},
+	middleware: [persistanceLocalStorageMiddleware],
 });
+
 store.subscribe(() => {
-	console.log(store.getState())
-  });
+	console.log(store.getState());
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
